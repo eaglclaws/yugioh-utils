aeda9a6616306c8d3468433412dbe45c087af624
Joseph Ko <eaglclaws@gmail.com>
2023-06-15 21:14:25 +0900

    Did weird magic shit to get card list

---
+++ scrape.py
@@ -0,0 +1,16 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+
+browser = webdriver.Firefox()
+browser.get("https://www.db.yugioh-card.com/yugiohdb/card_search.action")
+browser.find_element(By.ID, 'submit_area').click()
+time.sleep(2)
+browser.find_element(By.ID, 'mode_set').find_elements(By.TAG_NAME, 'li')[1].click()
+browser.get("https://www.db.yugioh-card.com/yugiohdb/card_search.action?ope=1&sess=2&rp=100&mode=2&stype=1&link_m=2&othercon=2&sort=1&request_locale=ko&page=1")
+
+soup = BeautifulSoup(browser.page_source, 'lxml')
+
+print(soup.find("div", {"id": "card_list"}).prettify())
+browser.quit()
